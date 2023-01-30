8a1e0672233d98b14707227aa0335155d374b187
Burrely <dinosammy1@live.nl>
2023-01-30 13:47:30 +0100

    Burrypack 1.3 update
    Webpage:
    - Added a title corresponding to the version and a very brief description of the contents of the page.
    - Added a basic Minecraft favicon hosted by a third party.
    - Created a date edited timestamp.
    
    Modpack:
    - Removed the shader from the modpack.
    - Added Optifine to the modpack.
    - Replaced Sophisticated Backpacks mod with the Backpacked mod.
    - Changed a bunch of config files which enable and disable certain features such as;
      - More modern behaviors.
      - Antique Atlas now shows a minimap when in the hotbar.
      - Disabled a bunch of join messages.
      - Removed a bunch of books that you'd usually spawn with by default.
      - Enabled Thaumcraft's vis crystals to regenerate.

--- BurryPack/datasheet manager.js
+++ BurryPack/datasheet manager.js
@@ -26,8 +26,8 @@ async function fetchDatasheet(url) {
 }
 
 function printDatasheetToConsole() {
-    console.log(datasheets["BurryPack 1.1"]);
-    datasheet = datasheets["BurryPack 1.1"];
+    console.log(datasheets["BurryPack 1.3"]);
+    datasheet = datasheets["BurryPack 1.3"];
 }
 
 function printDatasheetToDocument() {
